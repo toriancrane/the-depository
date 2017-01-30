e2a1192d01f0fe6a4516a71c020e6d52ad931ec5
toriancrane <toriancrane@gmail.com>
2017-01-30 11:19:16 -0700

    Created GUI Calculator interface and Tuition Calculator projects

---
+++ Tuition Calculator/tuition/TuitionConstants.java
@@ -0,0 +1,27 @@
+//**************************************************************************************************************
+// CLASS: TuitionConstants
+//
+// DESCRIPTION
+// Constants that are used in calculating the tuition for on-campus and online students. Use these constants
+// in the OnCampusStudent and OnlineStudent classes.
+//
+// AUTHOR
+// Kevin R. Burger (burgerk@asu.edu)
+// Computer Science & Engineering
+// School of Computing, Informatics, and Decision Systems Engineering
+// Fulton Schools of Engineering
+// Arizona State University, Tempe, AZ 85287-8809
+// Web: http://www.devlang.com
+//**************************************************************************************************************
+package tuition;
+
+public class TuitionConstants {
+
+    public static final int ONCAMP_ADD_CREDITS  = 350;
+    public static final int MAX_CREDITS         = 18;
+    public static final int ONCAMP_NONRES_BASE  = 12200;
+    public static final int ONCAMP_RES_BASE     = 5500;
+    public static final int ONLINE_CREDIT_RATE  = 875;
+    public static final int ONLINE_TECH_FEE     = 125;
+
+}
